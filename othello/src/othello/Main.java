6ca5274e49ff36d650631836a1d10e50c04d4848
hyuga <hyuga@localhost>
2023-11-25 12:35:39 +0900

    add Mainメソッド実行処理作成

--- othello/src/othello/Main.java
+++ othello/src/othello/Main.java
@@ -1,10 +1,23 @@
 package othello;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		
+		Board bd = new Board();
+		Scanner sc = new Scanner(System.in);
+		
+		while(bd.getGame()) {
+			
+			System.out.println(bd.getStone() + "のターンです。");
+			System.out.println("X座標を入力してください");
+			int x = sc.nextInt();
+			System.out.println("Y座標を入力してください");
+			int y = sc.nextInt();
+			
+			bd.setStone(--x,--y);
+		}
 	}
 
 }
